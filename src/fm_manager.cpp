accd6673bc09ce07ff7bf06498fbce5d0246a8b7
fmalekpour <fm@farhad.ca>
2020-11-25 23:08:14 -0800

    Cleanup

--- src/fm_manager.cpp
+++ src/fm_manager.cpp
@@ -2,7 +2,6 @@
 #include "grbl.h"
 #include "fm_lcd.h"
 #include "fm_system_data.h"
-#include "fm_twi.h"
 
 FMLcd *fm_manager_lcd;
 FMSystemData *fm_system_data;
@@ -11,8 +10,6 @@ uint32_t fm_manager_timer_counter;
 
 
 void fm_manager_setupTimer();
-void I2C_received(uint8_t received_data);
-void I2C_requested();
 
 
 void fm_manager_init()
@@ -23,11 +20,6 @@ void fm_manager_init()
 	fm_manager_timer_counter = 0;
 
 	fm_manager_setupTimer();
-
-	I2C_setCallbacks(I2C_received, I2C_requested);
-	I2C_init(0x04);
-
-
 }
 
 void fm_manager_setupTimer()
@@ -56,14 +48,10 @@ ISR(TIMER5_COMPA_vect)          // timer compare interrupt service routine
 	fm_manager_timer_counter++;
 	if(fm_manager_timer_counter>=10000)
 	{
-//	serial_write('.');
 		fm_manager_timer_counter = 0;
-//		fm_i2c_updateSystemData();
 		fm_system_data->update();
-//		fm_i2c_updateLcd();
 		fm_manager_lcd->updateLcd(fm_system_data);
 		fm_manager_lcd->mTimerStage = 0;
-//		return;
 	}
 
 	if(fm_manager_lcd->mTimerStage<100)
@@ -72,17 +60,3 @@ ISR(TIMER5_COMPA_vect)          // timer compare interrupt service routine
 	}
 }
 
-void I2C_received(uint8_t received_data)
-{
-}
-
-void I2C_requested()
-{
-	int s = sizeof(FM_SYSTEM_DATA);
-	uint8_t buf[sizeof(FM_SYSTEM_DATA)];
-	memcpy(buf, (uint8_t*)&(fm_system_data->mSystemData), s);
-	for(int i=0;i<s;i++)
-	{
-	  	I2C_transmitByte(buf[i]);
-	}
-}
